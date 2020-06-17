e904ff0a323fa4b5adf62badd2328ee3d6d91620
SongGyeongEun <62276767+SongGyeongEun@users.noreply.github.com>
2020-06-17 10:59:12 +0900

    Add files via upload

---
+++ radix.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a = 6;
+	
+	printf("hexa = %x octal = %o deciaml = %d integer = %i\n", a, a, a, a);
+	
+	return 0;
+}
+
---
+++ recursive1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int fact(int a)
+{
+	printf("%d\n", a);
+	if (a == 1)
+		return 1;
+	else
+		return a * fact(a-1);
+	printf("%d\n", a);
+}
+
+int main()
+{
+	int a;
+	
+	printf("숫자 입력 : ");
+	scanf("%d", &n);
+	a = fact(n);
+	
+	printf("%d! = %d\n", n, a);
+	
+	return 0;
+}
